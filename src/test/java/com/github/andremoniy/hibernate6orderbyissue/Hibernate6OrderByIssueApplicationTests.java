5068b223bc49291e34427da0a382c3fe23b16936
andremoniy2 <n/a>
2023-01-09 18:33:33 +0100

    First commit

--- src/test/java/com/github/andremoniy/hibernate6orderbyissue/Hibernate6OrderByIssueApplicationTests.java
+++ src/test/java/com/github/andremoniy/hibernate6orderbyissue/Hibernate6OrderByIssueApplicationTests.java
@@ -8,10 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Hibernate6OrderByIssueApplicationTests {
